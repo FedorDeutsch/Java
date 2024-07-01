97728b259cb368226ebe64d07d02ce37414fd13c
FedorProg <FedorGermanGut@gmail.com>
2024-07-01 09:09:41 +0300

    Update Lesson1.java

--- Lesson1.java
+++ Lesson1.java
@@ -1,14 +1,15 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
+        // Это базовые концепции языка
         System.out.println("Hello, World !"); // Теперь следий вывод будет с новой строки
         System.out.print("Hello, World !\n"); // Тоже перевод на новую строку(можно вставлять между словами)
         System.out.print("Hello \t World !"); // Табуляция
 
         /*
+
         Это многострочный комментарий
         Да, много строк :)
-         */
+
+        */
     }
 }
